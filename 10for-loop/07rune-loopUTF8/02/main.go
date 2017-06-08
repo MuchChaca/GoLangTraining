a98afa49ad6076f3884c245b6aaad22e56335fd3
archy <chkmake@gmail.com>
2017-06-08 18:12:22 +0000

    10for-loop: done

--- 10for-loop/07rune-loopUTF8/02/main.go
+++ 10for-loop/07rune-loopUTF8/02/main.go
@@ -7,4 +7,8 @@ func main() {
 		// Same 01 but with Printf()
 		fmt.Printf("%v - %v - %v\n", i, string(i), []byte(string(i)))
 	}
+	foo := 'a'
+	fmt.Println(foo)
+	fmt.Printf("%T \n", foo)
+	fmt.Printf("%T \n", rune(foo))
 }
