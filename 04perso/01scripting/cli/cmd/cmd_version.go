cbce672f425372995ed49b57f11c5baf5783a818
archey <chk-make@gmail.com>
2017-11-20 03:33:49 +0000

    :arrow up: ADDED make.sh    -> Build with different name

--- 04perso/01scripting/cli/cmd/cmd_version.go
+++ 04perso/01scripting/cli/cmd/cmd_version.go
@@ -31,7 +31,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show heft.io client version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(RootCmd.Use + " - v" + VERSION)
+		fmt.Println(RootCmd.Use + " - " + VERSION)
 	},
 }
 
---
+++ 04perso/01scripting/cli/make.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+go clean
+go build -o heft github.com/MuchChaca/GoLangTraining/04perso/01scripting/cli
