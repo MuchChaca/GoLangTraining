b4c56d99318286f6addc59886e2bc6d6b8773dc1
Hell <bastide.seb.pro@gmail.com>
2018-04-16 07:04:40 +0400

    made config/mongo.go - connection to db func

--- 04perso/03iris/04exp_test/authors/author.go
+++
@@ -1,8 +0,0 @@
-package authors
-
-// Author represents an author
-type Author struct {
-	ID    int64  `json:"id,omitempty"`
-	LName string `json:"lName"`
-	FName bool   `json:"fName"`
-}
--- 04perso/03iris/04exp_test/genres/genre.go
+++
@@ -1,8 +0,0 @@
-package genres
-
-// Genre represents a genre
-type Genre struct {
-	// SessionID string `json:"-"`
-	// ID    int64  `json:"id,omitempty"`
-	Label string `json:"label"`
-}
