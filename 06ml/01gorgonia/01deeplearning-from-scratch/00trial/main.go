e345e5db0c34d9d2fe73dd53a2192bee735356bd
ChacaS0 <chk-chaca@gmail.com>
2018-07-30 22:07:22 +0400

    :heavy_check_mark:      First example reproduced and kinda understood

--- 06ml/01gorgonia/01deeplearning-from-scratch/00trial/main.go
+++ 06ml/01gorgonia/01deeplearning-from-scratch/00trial/main.go
@@ -3,9 +3,28 @@ package main
 import (
 	"fmt"
 
-	. "gorgonia.org/gorgonia"
+	G "gorgonia.org/gorgonia"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// First we create a new graph
+	g := G.NewGraph()
+
+	// create a node called 'x' with the value 1
+	x := G.NodeFromAny(g, 1, G.WithName("x"))
+
+	// create a node called 'y' with the value 1
+	y := G.NodeFromAny(g, 1, G.WithName("y"))
+
+	// z = x + y
+	z := G.Must(G.Add(x, y))
+
+	// Create a VM to execute the graph
+	vm := G.NewTapeMachine(g)
+
+	// Run the VM... Errors are not checked
+	vm.RunAll()
+
+	// Print the value of z
+	fmt.Printf("%v\n", z.Value())
 }
